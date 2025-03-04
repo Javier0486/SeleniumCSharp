fea6c5b450cd7d2da057f4feae50467ae76bf8bb
Javier0486 <javiermm1986@gmail.com>
2025-03-04 13:10:08 -0600

    sending dictionary for homepage

---
+++ MiProjectoDePruebas/Utils/SwagLabsHomepageEnum.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+
+namespace SwagLabsHomepageEnum.Utils
+{
+    public static class ProductsInHomepage
+    {
+        public static Dictionary<string, string> Products = new Dictionary<string, string>
+        {
+            { "backPAck", "Sauce Labs Backpack" },
+            { "bikeLight", "Sauce Labs Bike Light" },
+            { "boltTshirt", "Sauce Labs Bolt T-Shirt" },
+            { "fleeceJacket", "Sauce Labs Fleece Jacket" },
+            { "onesie", "Sauce Labs Onesie" },
+            { "redTshirt", "Test.allTheThings() T-Shirt (Red)" }
+        };
+    }
+}
